ba8533b87c7da3946f6d216515d958202b653ee9
Johanna Palo <joa.palobael@gmail.com>
2021-12-17 11:17:29 -0300

    Terminado el front del formulario

---
+++ js/navbar.js
@@ -0,0 +1,28 @@
+let menuBtn = document.querySelector('#menuBtn');
+
+menuBtn.addEventListener('click', function () {
+  function navBar() {
+    let nav = document.getElementById("myTopnav");
+    if (nav.className == "topnav") {
+      nav.className += " responsive";
+    } else {
+      nav.className = "topnav";
+    }
+  }
+  function opacity(){
+    let opacidad = document.querySelector('.opacidad');
+    if(opacidad.className == "opacidad"){
+      opacidad.className += " paddingTop";
+    } else  {
+      opacidad.className = "opacidad";
+    }
+  }
+  navBar();
+  opacity();
+})
+
+
+
+
+
+
