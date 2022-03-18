80c2e5a63d4aa416e698219e070a873eaba2c3c3
Johanna Palo Bael <joa.palobael@gmail.com>
2022-03-18 12:01:41 -0300

    Se realizaron ajustes del curso

--- js/infoCurso.js
+++ js/infoCurso.js
@@ -12,48 +12,45 @@ num1.addEventListener('click', function () {
         `
     <h3 class="temario-titulo"><span>Bloque 1:</span> Criptomonedas, una tecnología revolucionaria</h3>
     <ul class="temario-lista">
-        <li>Introducción y Breve historia del dinero</li>
+        <li>Introducción y breve historia del dinero</li>
         <li>Recomendaciones antes de comenzar</li>
-        <li>¿Cualquiera puede aprender?</li>
-        <li>Bienvenidos a la disrupción: Bitcoin, una transformación fundamental del dinero </li>
+        <li>Criptomonedas ¿Cualquiera puede aprender?</li>
         <li>¿Qué son las criptomonedas y cómo surgen? </li>
-        <li>Proyectos anteriores a Bitcoin</li>
-        <li>Bitcoin: El activo digital más importante del Siglo XXI</li>
-        <li>¿Cómo se generan nuevos Bitcoin? </li>
-        <li>Criptomillonarios y primeros usuarios.</li>
-        <li>¿Es tarde para comenzar? proyección.</li>
-        <li>Principales mitos respecto a criptomonedas</li>
+        <li>Bienvenidos a la disrupción:<br> Bitcoin, una transformación fundamental del dinero</li>
+        <li>Conceptos clave: Blockchain, P2P, mecanismos de consenso y criptografía. </li>
+        <li>Intentos anteriores de dinero digital</li>
+        <li>BITCOIN: El activo digital más importante del siglo XXI </li>
+        <li>¿Por qué deberíamos prestarle  especial atención?</li>
+        <li>Emisión monetaria de Bitcoin </li>
+        <li>Criptomillonarios y early adopters.</li>
+        <li>¿Es tarde para comenzar? Proyección a futuro.</li>
+        <li>Principales mitos </li>
         <li>Estafas ¿cómo prevenirlas? </li>
         <li>Bitcoin como reserva de valor</li>
-        <li>Tecnologías fundacionales de Bitcoin</li>
-        <li><span>BONUS:</span> Videos explicativos, pdf's, libros, acceso a comunidad <a href="https://www.instagram.com/quierobit/">QuieroBit</a></li>
-</ul>
+        <li>Tecnologías fndacionales de bitcoin</li>
+    </ul>
       `
 })
 /*2*/
 num2.addEventListener('click', function () {
     temario.innerHTML =
         `
-    <h3 class="temario-titulo"><span>Bloque 2:</span> Conceptos claves </h3>
+    <h3 class="temario-titulo"><span>Bloque 2:</span> Conceptos claves y acción </h3>
     <ul class="temario-lista">
         <li>Internet de la información vs internet del valor</li>
         <li>Descentralización y Mecanismos de consenso.</li>
         <li>Tipos de criptomonedas, su clasificación y usos.</li>
-        <li>Fenomeno.com</li>
+        <li>Fenomeno ".com"</li>
         <li>Ethereum: Innovaciones tecnológicas de esta criptomoneda de 2da generación</li>
-        <li>Cómo adquirir tus primeras criptomonedas paso a paso</li>
+        <li>Consejos al momento de operar con criptomonedas</li>
         <li>Aspectos importantes a tener en cuenta al momento de invertir</li>
-        <li>Pasamos a la acción: Cómo comprar primeras criptomonedas y utilizar distintas herramientas y plataformas.</li>
+        <li>Pasamos a la acción:paso a paso cómo comprar nuestras primeras criptomonedas de forma simple</li>
         <li>Transacciones con criptomonedas</li>
-        <li>Errores comunes al comenzar a operar.</li>
+        <li>Herramientas y plataformas útiles.</li>
+        <li>Transacciones con criptomonedas</li>
+        <li>Errores comunes.</li>
         <li>Casos reales</li>
-        <li><span>BONUS:</span>
-            <ul>
-                <li>Manejo ordenes de mercado</li>
-                <li>Glosario Cripto</li>
-                <li>Test para conocer tu perfil como inversor</li>
-            </ul>
-        </li>
+        <li>Proyectos interesantes</li>
     </ul>
       `
 })
@@ -62,28 +59,29 @@ num2.addEventListener('click', function () {
 num3.addEventListener('click', function () {
     temario.innerHTML =
         `
-    <h3 class="temario-titulo"><span>Bloque 3:</span> Seguridad  y almacenamiento en criptomonedas + Formas de ganar dinero.
+    <h3 class="temario-titulo"><span>Bloque 3:</span> Seguridad, almacenamiento y formas de ganar dinero.
     </h3>
     <ul class="temario-lista">
-        <li>Todo lo que tenés que saber al momento de almacenar tus criptomonedas</li>
-        <li>Criptografía de clave asimétrica</li>
-        <li>Cómo ganar dinero operando con criptomonedas (Dentro del ecosistema cripto, las posibilidades son infinitas!)</li>
-        <li>Emociones al operar y ciclos de mercado </li>
-        <li>Recomendaciones y consejos útiles</li>
+        <li>Dónde guardar nuestras criptos de forma fácil y segura</li>
+        <li>Wallets</li>
+        <li>Criptografía de clave asimétrica.</li>
+        <li>Como ganar dinero con criptomonedas</li>
+        <li>Análisis técnico vs Análisis fundamental</li>
+        <li>Trading, distintas modalidades.</li>
+        <li>Psicología del trading</li>
+        <li>Emociones y ciclos de mercado </li>
+        <li>Stacking, yield farming y finanzas descentralizadas (DEFI)</li>
+        <li>NFT´s (Non fungible Tokens)</li>
+        <li>Plataformas y proyectos NFT´s</li>
+        <li>P2E -Play to earn (juega para ganar)-</li>
+        <li>Recomendaciones y consejos</li>
         <li>¿Por qué invertir en criptomonedas?</li>
         <li>Seguridad y criptomonedas</li>
         <li>Marcas y Metaversos</li>
-        <li>Tendencias en latinoamérica  </li>
-        <li>Cómo hacer networking</li>
+        <li>Tendencias cripto en latinoamérica  </li>
+        <li>Networking ¿Cómo hacer contactos?</li>
         <li>Aspecto importante: tomar acción.</li>
-        <li><span>BONUS:</span>
-            <ul>
-                <li>Libro estrategía trading</li>
-                <li>wallets recomendadas</li>
-                <li>glosario cripto</li>
-                <li>Excel para registro de operaciones</li>
-            </ul>
-        </li>
+        <li>Práctica e interacción</li>
     </ul>
       `
 })
@@ -92,22 +90,14 @@ num3.addEventListener('click', function () {
 num4.addEventListener('click', function () {
     temario.innerHTML =
         `
-    <h3 class="temario-titulo"><span>Bloque 4:</span> Tecnología en constante evolución</h3>
-    <ul class="temario-lista">
-        <li>Veremos aplicaciones casos de uso, aplicaciones, iniciativas y proyectos muy interesantes que dan uso de esta tecnología.</li>
-        <li>Wallets ¿Cúal elegir?</li>
-        <li>Indicadores que te permitirán entender cuándo es el mejor momento de comprar o vender tus criptomonedas. Una estrategia Simple y eficaz.</li>
-        <li>Alternativas para generar ingresos pasivos.</li>
-        <li>Finanzas descentralizadas DEFI.</li>
-        <li>Herramientas útiles disponibles en el mercado</li>
-        <li><span>BONUS:</span>
-            <ul>
-                <li>Material para profundizar</li>
-                <li>Acceso a bibliotecla cripto</li>
-                <li>Herramientas y contenido recomendado</li>
-            </ul>
-        </li>
-    </ul>
+    <h3 class="temario-titulo"><span>Bloque 4:</span> Encuentros con invitados</h3>
+    <div class="texto-centrado parrafo">
+    <p>Contaremos con la presencia de invitados: traders, emprendedores, comerciantes, artistas digitales, programadores, educadores, entusiastas, que compartirán su conocimiento y experiencias con nosotros.</p>
+    <br>
+    <p>Haremos encuentros con personas que tienen proyectos relacionados a las criptomonedas que nos contarán su cómo comenzaron, su, perspectiva y de sus distintos puntos de vista acerca del futuro del ecosistema cripto.</p>
+    <br>
+    <p>Haremos preguntas, despejaremos dudas y hablaremos sobre distintos proyectos, oportunidades y perspectivas a futuro</p>
+    </div>
       `
 })
 
@@ -115,15 +105,24 @@ num4.addEventListener('click', function () {
 num5.addEventListener('click', function () {
     temario.innerHTML =
         `
-    <h3 class="temario-titulo"><span>Bloque 5:</span> Encuentros con Invitados especiales</h3>
+    <h3 class="temario-titulo"><span>BENEFICIOS</span></h3>
     <ul class="temario-lista">
-        <li>Contaremos con la presencia de invitados especiales: traders, emprendedores, comerciantes, artistas, programadores, educadores, entusiastas, que compartirán sus conocimientos con nosotros. Haremos encuentros con personas que tienen proyectos relacionados a las criptomonedas, aprenderemos de su experiencia, perspectiva y de sus distintos puntos de vista acerca del futuro del ecosistema cripto. Haremos preguntas, despejaremos dudas y hablaremos sobre distintos proyectos, oportunidades, y perspectivas a futuro</li>
-        <li>
-        <span>BONUS:</span>
-            <ul>
-                <li>Daremos acceso a una comunidad privada dónde compartimos información y brindamos apoyo, estableciendo así una red de contactos saludable que los ayude a crecer y dónde puedas despejar tus dudas y consultas!</li>
-                <li>Biblioteca Cripto  y newsletter mensual para que estés al tanto de información relevante en el mercado de las criptomonedas!</li>
+        <li>Acceso a nuestra plataforma dónde encontrarán mucho contenido:
+            <ul class="temario-sublista">
+                <li>Biblioteca #Quierobit (Libros de criptomonedas,trading, finanzas, inversiones y más)
+                <li>Tutoriales (Órdenes de compra, manejo de plataforma, wallets)
+                <li>Videos explicativos, entrevistas, documentales, películas recomendadas
+                <li>Acceso a las charlas con los invitados especiales de las ediciones anteriores
+                <li>Herramientas útiles, wallets y contenido relevante
+            </ul>
+        </li>
+        <li>Acceso a una comunidad privada de whatsapp dónde compartimos información y brindamos apoyo, estableciendo así una red de contactos saludable que los ayude a crecer y dónde puedas despejar tus dudas y consultas!</li>
+        <li>Estarás suscripto a nuestro Newsletter mensual para que estés al tanto de información, oportunidades y contenido relevante en el mercado de las criptomonedas!</li>
+
+        <li>Beneficio exclusivo
+            <ul class="temario-sublista">
                 <li>1 mes gratis en el grupo de Alertas de trading #QuieroVip!</li>
+                <li>Libro la “estrategia de las 5 puntas” utilizada por #Quierobit</li>
             </ul>
         </li>
     </ul>
