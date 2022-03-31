dfe80fe123cba262f407d864bbb9853e15593145
Johanna Palo Bael <joa.palobael@gmail.com>
2022-03-30 21:43:26 -0300

    Agregue testimonials en Dispositivos grandes

--- js/testimonialPagination.js
+++ js/testimonialPagination.js
@@ -1,5 +1,6 @@
 /*Guardar en variables elementos a modificar del DOM*/
-let testimonials = document.querySelector('.testimonials');
+let testimonialsSM = document.querySelector('.testimonials-sm');
+let testimonialsXL = document.querySelector('.testimonials-xl');
 let circle1SM = document.querySelector('.circle1-sm');
 let circle2SM = document.querySelector('.circle2-sm');
 let circle3SM = document.querySelector('.circle3-sm');
@@ -13,12 +14,18 @@ let circle10SM = document.querySelector('.circle10-sm');
 let circle11SM = document.querySelector('.circle11-sm');
 let circle12SM = document.querySelector('.circle12-sm');
 let circle13SM = document.querySelector('.circle13-sm');
+let circle1XL = document.querySelector('.circle1-xl');
+let circle2XL = document.querySelector('.circle2-xl');
+let circle3XL = document.querySelector('.circle3-xl');
+let circle4XL = document.querySelector('.circle4-xl');
+let circle5XL = document.querySelector('.circle5-xl');
 
+// Dispositivos Chicos
 /*1 - Lean Lalia*/
 circle1SM.addEventListener('click', function () {
-    testimonials.innerHTML =
+    testimonialsSM.innerHTML =
         `
-    <figure class="sm testimonial ">
+    <figure class="testimonial ">
         <figcaption>
             <blockquote>
                 <p>
@@ -37,105 +44,106 @@ circle1SM.addEventListener('click', function () {
 
 /*2 - Crismara*/
 circle2SM.addEventListener('click', function () {
-    testimonials.innerHTML =
+    testimonialsSM.innerHTML =
         `
-    <figure class="sm testimonial">
-    <figcaption>
-      <blockquote>
-        <p>
-            Gracias QuieroBit! <br>
-            Muy bueno el curso, tanto la teoría como la práctica. <br>
-            Y lo mejor es el aliento a pasar a la acción..
-        </p>
-      </blockquote>
-      <h3>Crismara</h3>
-      <h4><a href="https://www.instagram.com/crismaraparaarmar/"  target="_blank">@crismaraparaarmar</a></h4>
-    </figcaption>
+    <figure class="testimonial">
+        <figcaption>
+        <blockquote>
+            <p>
+                Gracias QuieroBit! <br>
+                Muy bueno el curso, tanto la teoría como la práctica. <br>
+                Y lo mejor es el aliento a pasar a la acción..
+            </p>
+        </blockquote>
+        <h3>Crismara</h3>
+        <h4><a href="https://www.instagram.com/crismaraparaarmar/"  target="_blank">@crismaraparaarmar</a></h4>
+        </figcaption>
+    </figure>
     `
 });
 
 /*3 - Pablo M*/
 circle3SM.addEventListener('click', function () {
-    testimonials.innerHTML =
+    testimonialsSM.innerHTML =
         `
-        <figure class="sm testimonial">
-        <figcaption>
-          <blockquote>
-            <p>
-            Una de la mejores inversiones para empezar en el mundo Cripto. <br>
-            De fácil comprensión y mucha onda de parte del orador, un crack!
-            </p>
-          </blockquote>
-          <h3>Pablo M.</h3>
-          <h4><a href="https://www.instagram.com/pablom89/"  target="_blank">@Pablom89</a></h4>
-        </figcaption>
-    </figure>
+        <figure class="testimonial">
+            <figcaption>
+                <blockquote>
+                    <p>
+                    Una de la mejores inversiones para empezar en el mundo Cripto. <br>
+                    De fácil comprensión y mucha onda de parte del orador, un crack!
+                    </p>
+                </blockquote>
+                <h3>Pablo M.</h3>
+                <h4><a href="https://www.instagram.com/pablom89/"  target="_blank">@Pablom89</a></h4>
+            </figcaption>
+        </figure>
     `
 });
 
 /*4 - Jose Bitti*/
 circle4SM.addEventListener('click', function () {
-    testimonials.innerHTML =
+    testimonialsSM.innerHTML =
         `
         <figure class="sm testimonial">
-        <figcaption>
-          <blockquote>
-            <p>
-            Quiero felicitar a Matías y a su equipo por  la predisposición y buena onda. <br>
-            El curso muy claro y practico, lo recomiendo 100%! <br>
-            Sigan así, éxitos!
-            </p>
-          </blockquote>
-          <h3>José Bitti</h3>
-          <h4><a href="https://www.instagram.com/josebitti/"  target="_blank">@josebitti</a></h4>
-        </figcaption>
-    </figure>
+            <figcaption>
+            <blockquote>
+                <p>
+                Quiero felicitar a Matías y a su equipo por  la predisposición y buena onda. <br>
+                El curso muy claro y practico, lo recomiendo 100%! <br>
+                Sigan así, éxitos!
+                </p>
+            </blockquote>
+            <h3>José Bitti</h3>
+            <h4><a href="https://www.instagram.com/josebitti/"  target="_blank">@josebitti</a></h4>
+            </figcaption>
+        </figure>
     `
 });
 
 /*5 - Ximena Merelle*/
 circle5SM.addEventListener('click', function () {
-    testimonials.innerHTML =
+    testimonialsSM.innerHTML =
         `
         <figure class="sm testimonial">
-        <figcaption>
-          <blockquote>
-            <p>
-            Empecé el curso sin tener idea que era una criptomoneda y hoy ando diciéndole a todo el mundo que tienen que meterse al mundo Bitcoin ya! <br>
-            Solo agradecerles por ayudarme a animarme.
-            </p>
-          </blockquote>
-          <h3>Ximena Merelle</h3>
-          <h4><a href="https://www.instagram.com/ximena_merelle/  target="_blank"">@ximena_merelle</a></h4>
-        </figcaption>
-    </figure>
+            <figcaption>
+            <blockquote>
+                <p>
+                Empecé el curso sin tener idea que era una criptomoneda y hoy ando diciéndole a todo el mundo que tienen que meterse al mundo Bitcoin ya! <br>
+                Solo agradecerles por ayudarme a animarme.
+                </p>
+            </blockquote>
+            <h3>Ximena Merelle</h3>
+            <h4><a href="https://www.instagram.com/ximena_merelle/  target="_blank"">@ximena_merelle</a></h4>
+            </figcaption>
+        </figure>
     `
 });
 
 /*6 - Anonimo*/
 circle6SM.addEventListener('click', function () {
-    testimonials.innerHTML =
+    testimonialsSM.innerHTML =
         `
         <figure class="sm testimonial">
-        <figcaption>
-          <blockquote>
-            <p>
-            El curso superó mis expectativas. <br>
-            Mati y su equipo supieron plasmar y ordenar una variedad de temas del mundo Cripto en su curso y en Notion.<br>
-            Buscaba exactamente lo que me dieron. <br>
-            Otra  cosa a destacar el lado humano y el continuo seguimiento y predisposición de Matías y de su equipo para responder todas las dudas. <br>
-            Agradecido y me voy a sumar al curso de trading también.
+            <figcaption>
+            <blockquote>
+                <p>
+                El curso superó mis expectativas. <br>
+                Mati y su equipo supieron plasmar y ordenar una variedad de temas del mundo Cripto en su curso y en Notion.<br>
+                Buscaba exactamente lo que me dieron. <br>
+                Otra  cosa a destacar el lado humano y el continuo seguimiento y predisposición de Matías y de su equipo para responder todas las dudas. <br>
+                Agradecido y me voy a sumar al curso de trading también.
 
-          </blockquote>
-          <h3>Anónimo</h3>
-        </figcaption>
-    </figure>
+            </blockquote>
+            <h3>Anónimo</h3>
+            </figcaption>
+        </figure>
     `
 });
 
 /*7 - Leo Cavs */
 circle7SM.addEventListener('click', function () {
-    testimonials.innerHTML =
+    testimonialsSM.innerHTML =
         `
         <figure class="sm testimonial">
         <figcaption>
@@ -153,7 +161,7 @@ circle7SM.addEventListener('click', function () {
 
 /*8 - Diana Torres*/
 circle8SM.addEventListener('click', function () {
-    testimonials.innerHTML =
+    testimonialsSM.innerHTML =
         `
         <figure class="sm testimonial">
         <figcaption>
@@ -173,7 +181,7 @@ circle8SM.addEventListener('click', function () {
 
 /*9 - Ale Lenti*/
 circle9SM.addEventListener('click', function () {
-    testimonials.innerHTML =
+    testimonialsSM.innerHTML =
         `
         <figure class="sm testimonial">
         <figcaption>
@@ -194,7 +202,7 @@ circle9SM.addEventListener('click', function () {
 
 /*10 - Adriana Pasqualini */
 circle10SM.addEventListener('click', function () {
-    testimonials.innerHTML =
+    testimonialsSM.innerHTML =
         `
         <figure class="sm testimonial">
         <figcaption>
@@ -217,7 +225,7 @@ circle10SM.addEventListener('click', function () {
 
 /*11 - Emi Montero*/
 circle11SM.addEventListener('click', function () {
-    testimonials.innerHTML =
+    testimonialsSM.innerHTML =
         `
         <figure class="sm testimonial">
         <figcaption>
@@ -239,7 +247,7 @@ circle11SM.addEventListener('click', function () {
 
 /*12 */
 circle12SM.addEventListener('click', function () {
-    testimonials.innerHTML =
+    testimonialsSM.innerHTML =
         `
         <figure class="sm testimonial">
         <figcaption>
@@ -258,7 +266,7 @@ circle12SM.addEventListener('click', function () {
 
 /*13 */
 circle13SM.addEventListener('click', function () {
-    testimonials.innerHTML =
+    testimonialsSM.innerHTML =
         `
         <figure class="sm testimonial">
         <figcaption>
@@ -276,5 +284,210 @@ circle13SM.addEventListener('click', function () {
           <h4><a href="https://www.instagram.com/yami_carauni/" target="_blank">@yami_carauni</a></h4>
         </figcaption>
     </figure>
+    `
+});
+
+//Dispositivos Grandes
+circle1XL.addEventListener('click', function () {
+    testimonialsXL.innerHTML =
+        `
+        <figure class="testimonial ">
+        <figcaption>
+            <blockquote>
+                <p>
+                    Destaco la claridad y paciencia a la hora de capacitar. <br>
+                    Excelente material y preparación de las clases. <br>
+                    Muy positivo la participación de invitados. <br>
+                    Felicitaciones, un 10.
+                </p>
+            </blockquote>
+            <h3>Leandro Lalia</h3>
+            <h4><a href="https://www.instagram.com/lean_lalia/" target="_blank">@lean_lalia</a></h4>
+        </figcaption>
+    </figure>
+    <figure class="testimonial">
+        <figcaption>
+          <blockquote>
+            <p>
+                Gracias QuieroBit! <br>
+                Muy bueno el curso, tanto la teoría como la práctica. <br>
+                Y lo mejor es el aliento a pasar a la acción..
+            </p>
+          </blockquote>
+          <h3>Crismara</h3>
+          <h4><a href="https://www.instagram.com/crismaraparaarmar/"  target="_blank">@crismaraparaarmar</a></h4>
+        </figcaption>
+    </figure>
+    <figure class="testimonial">
+        <figcaption>
+            <blockquote>
+                <p>
+                Una de la mejores inversiones para empezar en el mundo Cripto. <br>
+                De fácil comprensión y mucha onda de parte del orador, un crack!
+                </p>
+            </blockquote>
+            <h3>Pablo M.</h3>
+            <h4><a href="https://www.instagram.com/pablom89/"  target="_blank">@Pablom89</a></h4>
+        </figcaption>
+    </figure>
+    `
+});
+
+circle2XL.addEventListener('click', function () {
+    testimonialsXL.innerHTML =
+        `
+        <figure class="sm testimonial">
+            <figcaption>
+            <blockquote>
+                <p>
+                Quiero felicitar a Matías y a su equipo por  la predisposición y buena onda. <br>
+                El curso muy claro y practico, lo recomiendo 100%! <br>
+                Sigan así, éxitos!
+                </p>
+            </blockquote>
+            <h3>José Bitti</h3>
+            <h4><a href="https://www.instagram.com/josebitti/"  target="_blank">@josebitti</a></h4>
+            </figcaption>
+        </figure>
+        <figure class="sm testimonial">
+            <figcaption>
+            <blockquote>
+                <p>
+                Empecé el curso sin tener idea que era una criptomoneda y hoy ando diciéndole a todo el mundo que tienen que meterse al mundo Bitcoin ya! <br>
+                Solo agradecerles por ayudarme a animarme.
+                </p>
+            </blockquote>
+            <h3>Ximena Merelle</h3>
+            <h4><a href="https://www.instagram.com/ximena_merelle/  target="_blank"">@ximena_merelle</a></h4>
+            </figcaption>
+        </figure>
+        <figure class="sm testimonial">
+            <figcaption>
+            <blockquote>
+                <p>
+                El curso superó ampliamente mis expectativas. <br>
+                Mati y su equipo supieron plasmar y ordenar una variedad de temas del mundo Cripto en su curso y en Notion.<br>
+                A destacar el lado humano y el continuo seguimiento y predisposición para responder todas las dudas. <br>
+                Me voy a sumar al curso de trading también.
+
+            </blockquote>
+            <h3>Anónimo</h3>
+            </figcaption>
+        </figure>
+    `
+});
+
+circle3XL.addEventListener('click', function () {
+    testimonialsXL.innerHTML =
+    `
+    <figure class="testimonial">
+        <figcaption>
+            <blockquote>
+                <p>
+                Quiero ver los Mati NFT
+                </p>
+            </blockquote>
+            <h3>Leo Cavs</h3>
+            <h4><a href="https://www.instagram.com/Leo._.cavs/"  target="_blank">@Leo._.cavs </a></h4>
+        </figcaption>
+    </figure>
+    <figure class="testimonial">
+        <figcaption>
+            <blockquote>
+                <p>
+                Muy comprometidos en lograr que los estudiantes entiendan los temas, considerando que el vocabulario puede ser complejo al inicio. <br>
+                Un 100%!!
+
+                </p>
+            </blockquote>
+            <h3>Diana Torres</h3>
+        <h4><a href="https://www.instagram.com/dianatorrescr/" target="_blank">@dianatorrescr</a></h4>
+        </figcaption>
+    </figure>
+    <figure class="testimonial">
+        <figcaption>
+            <blockquote>
+                <p>
+                Quiero felicitarlos porque tanto el contenido como la manera de dictar el curso es muy bueno. <br>
+                De fácil entendimiento para aquellos que jamás escucharon hablar sobre cripto. <br>
+                Sigan así!!!
+
+                </p>
+            </blockquote>
+            <h3>Ale Lenti</h3>
+            <h4><a href="https://www.instagram.com/ale.lenti_/" target="_blank">@ale.lenti_ </a></h4>
+        </figcaption>
+    </figure>
+    `
+});
+
+circle4XL.addEventListener('click', function () {
+    testimonialsXL.innerHTML =
+    `
+    <figure class="sm testimonial">
+    <figcaption>
+      <blockquote>
+        <p>
+        Me pareció una excelente curso para principiantes o nivel intermedio. <br>
+        Matías tiene una capacidad para explicar clarísima, es muy amable y muy predispuesto. <br>
+        El curso es abarcativo pero no excesivo. Si necesitas profundizar sobre un tema se puede hacer perfectamente. <br>
+        Me encantó! <br>
+        Lo recomiendo ampliamente!
+
+        </p>
+      </blockquote>
+      <h3>Adriana Pasqualini</h3>
+      <h4><a href="https://www.instagram.com/Adri_pasqualini/" target="_blank">@Adri_pasqualini</a></h4>
+    </figcaption>
+</figure>
+<figure class="sm testimonial">
+<figcaption>
+  <blockquote>
+    <p>
+    Adentrarse al mundo desconocido de las Cripto de la mano de Quierobit fue mi mejor decisión. <br>
+    Cálidos, emprendedores y extremadamente generosos de compartir información y conocimiento (bienes tan preciados hoy en día). <br<
+    Profundamente agradecida. <br>
+    Éxitos en el camino por recorrer.
+
+    </p>
+  </blockquote>
+  <h3>Emi Montero</h3>
+  <h4><a href="https://www.instagram.com/emimontero/"  target="_blank">@emimontero</a></h4>
+</figcaption>
+</figure>
+<figure class="sm testimonial">
+<figcaption>
+  <blockquote>
+    <p>
+    Estoy muy conforme con la manera en cómo se explican los temas en las clases y seguro que cada curso que vayan haciendo todo irá saliendo mejor aun! <br>
+    Muchas gracias!!!
+
+    </p>
+  </blockquote>
+  <h3>Alexander Cardona</h3>
+</figcaption>
+</figure>
+    `
+});
+
+circle5XL.addEventListener('click', function () {
+    testimonialsXL.innerHTML =
+    `
+    <figure class="sm testimonial">
+    <figcaption>
+      <blockquote>
+        <p>
+        Muy buen curso, por supuesto que lo recomiendo. <br>
+        Está más que claro que tienen como objetivo brindar conocimiento y no quedarselo. <br>
+        Mati lo hace muy ameno, en la plataforma dejan mucha info. <br>
+        Los invitados también unos genios. <br>
+        Me sentí muy cómoda con el tema de preguntar y la distensión en el grupo de WhatsApp. <br> Espero que la sigan rompiendo!!
+
+        </p>
+      </blockquote>
+      <h3>Yami Carauni</h3>
+      <h4><a href="https://www.instagram.com/yami_carauni/" target="_blank">@yami_carauni</a></h4>
+    </figcaption>
+</figure>
     `
 });
